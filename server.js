2ba6aef229bf1878fe67dc79b786c34903e14e1e
Phollet <phollet.kh.new@gmail.com>
2026-04-27 02:41:22 +0700

    serve public folder

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
-
+const path = require('path');
 const app = express();
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'public')));
 // Track processed messages to avoid duplicates
 const processedMessages = new Set();
 
