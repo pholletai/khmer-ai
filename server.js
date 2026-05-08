49100ca6d90862181f4dd0a0a5680beaa2cadf59
Phollet <phollet.kh.new@gmail.com>
2026-05-09 00:46:06 +0700

    fix payload limit 50mb for voice

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ const {
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // =========================
 // API: POST /chat
